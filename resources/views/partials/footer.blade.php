855d1aef0e2809d2e3d9fdcc882cbecb99dd92ac
Hermansyah <hermansyah@Hermansyahs-MacBook-Air.local>
2025-01-03 01:01:50 +0700

    copyright di footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,10 @@
 <footer id="footer" class="footer position-relative">
     <div class="footer-newsletter">
-      
+        <!-- Konten Newsletter atau bagian lainnya tetap kosong jika tidak digunakan -->
     </div>
-    <!--  -->
+
+    <div class="container text-center mt-4">
+        <p>© Copyright <strong>Semesta.org</strong>. All Rights Reserved</p>
+        <p>Powered by <a href="https://hermanspace.id" target="_blank">Hermanspace.id</a></p>
+    </div>
 </footer>
