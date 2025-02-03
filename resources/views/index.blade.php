5f0db83e922144cd19836e98b638ae539ce86d9c
Herman <hermansyah_@live.com>
2025-02-03 21:46:42 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -490,26 +490,12 @@
 
 
 <!-- ========================= JS here ========================= -->
-<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+<!-- <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script> -->
 <script src="{{ asset('assets/js/wow.min.js') }}"></script>
-<script src="{{ asset('assets/js/tiny-slider.js') }}"></script>
-<script src="{{ asset('assets/js/count-up.min.js') }}"></script>
+<!-- <script src="{{ asset('assets/js/tiny-slider.js') }}"></script> -->
+<!-- <script src="{{ asset('assets/js/count-up.min.js') }}"></script> -->
 <!-- <script src="{{ asset('assets/js/main.js') }}"></script> -->
-<script>
-    //========= testimonial 
 
-
-    //====== counter up 
-    var cu = new counterUp({
-        start: 0,
-        duration: 2000,
-        intvalues: true,
-        interval: 100,
-        append: " ",
-    });
-    cu.start();
-
-</script>
 
 <script>
     $(document).ready(function () {
