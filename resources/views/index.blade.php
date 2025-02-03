7e0f5d2478b33a0d4efc10964b3a0646b0cb7638
Herman <hermansyah_@live.com>
2025-02-03 21:40:51 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -494,7 +494,7 @@
 <script src="{{ asset('assets/js/wow.min.js') }}"></script>
 <script src="{{ asset('assets/js/tiny-slider.js') }}"></script>
 <script src="{{ asset('assets/js/count-up.min.js') }}"></script>
-<script src="{{ asset('assets/js/main.js') }}"></script>
+<!-- <script src="{{ asset('assets/js/main.js') }}"></script> -->
 <script>
     //========= testimonial 
 
